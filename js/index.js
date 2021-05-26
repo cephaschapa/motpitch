95a2c4d4ea3a795a4dfe9d489ca62352331563b2
Cephas Chapa <cephaschapa@gmail.com>
2021-05-26 21:34:36 +0200

    done 90%

--- js/index.js
+++ js/index.js
@@ -34,10 +34,28 @@ $(document).ready(function(){
     }).then((res=>{
         console.log(res)
         let userid = res.data.data._id
+        let details = res.data.data
         Cookies.set('userid',userid)
+        $('#profile-box').html(`
+                <div class="card">
+                <div class="card-image">
+                    <img src="./assets/images/pitch.jpg"/>
+                </div>
+                <div class="card-content">
+                    <div class="card-title">
+                        ${details.username}
+                    </div>
+                    <p>${details.firstname} ${details.lastname}</p>
+                    <p>${details.mobile}</p>
+                    <p>${details.email}</p>
+                </div>
+            </div>
+        `)
 
     })).catch(err=>console.error(err))
     let userId = Cookies.get('userid')
+
+
     axios({
         method: 'get',
         url: `https://mosioatunya.herokuapp.com/api/v1/pitch/user/${userId}`,
@@ -49,15 +67,24 @@ $(document).ready(function(){
         let data = res.data.data
         data.forEach(element => {
             let output = `
+            <div class="col s12 m3 mx-auto">
             <div class="card">
+                <div class="card-image">
+                    <img src="./assets/images/pitch.jpg"/>
+                </div>
                 <div class="card-content">
                     <div class="card-title">
                         ${element.title}
                     </div>
+                    <span>${element.description}</span>
+                    <span>${element.pitchvideo}</span>
+                    <span>${element.groupmembers}</span>
                 </div>
-            </div>`
+            </div>
+            </div>
+            `
             console.log(output)
-            if(!output && !token){
+            if(data.length==0 && !token){
                 $('#card-render-box').html(`
                     <p>Please login to submit a pitch</p>
                 `)
@@ -68,7 +95,7 @@ $(document).ready(function(){
         
     })).catch(err=>console.error(err))
 
-    console.log(token)
+    // console.log(token)
     if(token){
         $('#acc-box').html(`
             <li>
@@ -77,6 +104,17 @@ $(document).ready(function(){
                     Logout
                 </a>
             </li>
+        `)
+        $('#dynamic-btn').html(`
+            
+            <a href="./profile.html" class="white-text"><i class="material-icons">person</i></a>
+            
+        `)
+        $('#account-ctrl').html(`
+        <a id="logout-h" class="waves-effect" href="./login.html" style="float:right; right:-100px;top:-63px"
+          ><i class="material-icons left">power_settings_new</i>Logout</a
+        >
+        
         `)
     }
     $('#login-form').submit(function(e){
@@ -116,6 +154,21 @@ $(document).ready(function(){
             window.location.reload()
         })).catch(err=>console.error(err))
     })
+    $('#logout-h').click(function(e){
+        e.preventDefault()
+        let token = Cookies.get('token')
+        axios({
+            method: 'get',
+            url: 'https://mosioatunya.herokuapp.com/api/v1/auth/logout',
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then((res=>{
+            console.log(res)
+            Cookies.set('token','')
+            window.location.reload()
+        })).catch(err=>console.error(err))
+    })
     
     $('#submit-pitch').click(function(e){
         e.preventDefault()
@@ -140,10 +193,62 @@ $(document).ready(function(){
             }
         }).then((res=>{
             console.log(res)
-            // window.location.reload()
+            window.location.reload()
             
             
 
         })).catch(err=>console.error(err))
     })
+    $('#login-form').submit(function(e){
+        e.preventDefault();
+        let email = $('#email-b').val()
+        let password = $('#password').val()
+
+        console.log(email, password)
+        axios({
+            method: 'post',
+            url: "https://mosioatunya.herokuapp.com/api/v1/auth/login",
+            data: {
+                email: email,
+                password: password
+            },
+        }).then((result) => {
+            Cookies.set('token', `${result.data.token}`)
+            let token = Cookies.get('token')
+            window.location.replace('/index.html')
+        }).catch((err) => {
+            
+        });
+    })
+
+    $('#signin-form').submit(function(e){
+        e.preventDefault()
+        let username = $('#username').val()
+        let fname = $('#fname').val()
+        let lname = $('#lname').val()
+        let mobile = $('#mobile').val()
+        let email = $('#email').val()
+        let password = $('#password').val()
+
+        console.log($(this))
+        axios({
+            method: 'post',
+            url: "https://mosioatunya.herokuapp.com/api/v1/auth/register",
+            data: {
+                username:username,
+                firstname: fname,
+                lastname: lname,
+                mobile:mobile,
+                email: email,
+                password: password
+            },
+        }).then((result) => {
+            Cookies.set('token', `${result.data.token}`)
+            let token = Cookies.get('token')
+            window.location.replace('/index.html')
+        }).catch((err) => {
+            alert('Something went wrong')
+        });
+    })
+
 })
