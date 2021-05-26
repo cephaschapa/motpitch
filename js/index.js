4e51fb045b0f39f2573600f09fe4f7acc312de1f
Cephas Chapa <cephaschapa@gmail.com>
2021-05-26 22:03:16 +0200

    ALl

--- js/index.js
+++ js/index.js
@@ -215,9 +215,9 @@ $(document).ready(function(){
         }).then((result) => {
             Cookies.set('token', `${result.data.token}`)
             let token = Cookies.get('token')
-            window.location.replace('/index.html')
+            window.location.replace('https://cephaschapa.github.io/motpitch/')
         }).catch((err) => {
-            
+            alert('Invalid Credentials')
         });
     })
 
